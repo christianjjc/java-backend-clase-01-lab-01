bc38d849857dcdd1f1e145368db1c56cf7fb84af
christianjjc <christianjjc@gmail.com>
2024-03-12 10:46:12 -0500

    initial commit

---
+++ src/main/java/edu/pe/cibertec/Ejecutora.java
@@ -0,0 +1,23 @@
+package edu.pe.cibertec;
+
+import edu.pe.cibertec.beans.HolaMundo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Ejecutora {
+    public static void main(String[] args) {
+
+        // Proramación Tradicional
+        /**
+        HolaMundo holamundo = new HolaMundo();
+        holamundo.setNombre("Pepito Perez");
+        holamundo.saludar();
+        **/
+
+        // Programando como indica SPRING
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HolaMundo holamundo = (HolaMundo) context.getBean("holaMundoBean");
+        holamundo.saludar();
+
+    }
+}
